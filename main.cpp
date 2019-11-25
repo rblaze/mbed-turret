2311f92f7366ada32425b7d06634d8cae2889aad
Andrey Sverdlichenko <blaze@rusty.zone>
2019-11-24 23:07:01 -0500

    Add servo range adjustment

--- main.cpp
+++ main.cpp
@@ -11,13 +11,35 @@ PinName SENSOR_SERVO{D6};
 PinName LASER_SERVO{D5};
 PinName LASER_POWER{D4};
 PinName AUDIO_OUT{PB_11};
-// PinName SERVO_RANGE_POT{A0};
+PinName SERVO_RANGE_POT{A0};
 
-constexpr float sensorServoRange{0.0009};
-constexpr float sensorServoAngle{90};
+constexpr float servoRangeMin{0.0003};
+constexpr size_t servoAngleMin{30};
 
-constexpr float laserServoRange{0.0009};
-constexpr float laserServoAngle{90};
+constexpr float servoRangeMax{0.0009};
+constexpr size_t servoAngleMax{90};
+
+size_t calibrateServos(FastServo &sensor, FastServo &laser) {
+  AnalogIn pot{SERVO_RANGE_POT};
+  float pct{pot.read()};
+
+  // Force end values.
+  if (pct > 0.99) {
+    pct = 1;
+  } else if (pct < 0.01) {
+    pct = 0;
+  }
+
+  float range = servoRangeMin + (servoRangeMax - servoRangeMin) * pct;
+  float angle = servoAngleMin + (servoAngleMax - servoAngleMin) * pct;
+
+  printf("calibration %f -> %f %f\n", pct, range, angle);
+
+  sensor.calibrate(range, angle);
+  laser.calibrate(range, angle);
+
+  return angle;
+}
 
 int main() {
   I2C i2c{SENSOR_SDA, SENSOR_SCL};
@@ -27,12 +49,10 @@ int main() {
   DigitalOut laser{LASER_POWER, 0};
   AudioPlayer audio{AUDIO_OUT};
   TargetPainter painter{laserServo, laser, audio};
-
-  //  sensorServo.calibrate(sensorServoRange, sensorServoAngle);
-  //  laserServo.calibrate(laserServoRange, laserServoAngle);
+  auto numSteps{calibrateServos(sensorServo, laserServo) * 2};
 
   audio.play(AudioPlayer::Clip::DEPLOYING);
-  RangingScanner scanner(sensorServo, sensor, 90, 2, 100);
+  RangingScanner scanner(sensorServo, sensor, numSteps, 2, 100);
 
   audio.play(AudioPlayer::Clip::SFX_DEPLOY);
   for (;;) {
